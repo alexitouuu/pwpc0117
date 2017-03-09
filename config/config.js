956ef6e3f8591b014e694e73543b903421b86b5a
alexitouuu <alexinlop@gmail.com>
2017-03-09 08:16:33 -0600

    00 Iniciando Proyecto de Server

---
+++ config/config.js
@@ -0,0 +1,11 @@
+exports.module = {
+    "IP": "127.0.0.1" || process.env.IP,
+    "PORT": 3000 || process.env.PORT,
+    "color_theme" : {
+        "info" : "ranbow",
+        "data" : "greem",
+        "error" : "red",
+        "warning": "yellow"
+    },
+    "SATIC_PATH": "./static"
+};
---
+++ static/js/site.js
@@ -0,0 +1,4 @@
+//Este script sera ejecutado por el cliente (Navegador)
+function saludar(){
+    alert("Bienvenido a mi sitio");
+}
